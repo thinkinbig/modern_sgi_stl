25792f0d4adc9422c1a85663a7fa71ea0c8f4ea1
Zeyu Li <go72wog@mytum.de>
2025-04-30 19:45:02 +0200

    add lru cache

---
+++ mstl_lru.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <unordered_map>
+#include <iostream>
+#include <list>
+#include <stdexcept>
+
+namespace mstl {
+
+    template <typename Key, typename Value>
+    class LRUCache {
+    private:
+        struct Node {
+            Key key;
+            Value value;
+            Node(Key key, Value value) : key(key), value(value) { }
+        };
+
+        int capacity_;
+        std::list<Node> cache_;
+        std::unordered_map<Key, typename std::list<Node>::iterator> map_;
+
+
+    public:
+        LRUCache(int capacity): capacity_(capacity), cache_(), map_() { }
+
+        Value get(Key key) {
+            auto it = map_.find(key);
+            if (it == map_.end()) {
+                throw std::range_error("Key not found in cache");
+            }
+
+            // Move accessed node to front
+            cache_.splice(cache_.begin(), cache_, it->second);
+            return it->second->value;
+        }
+
+        void put(const Key& key, const Value& value) {
+            auto it = map_.find(key);
+
+            if (it != map_.end()) {
+                // Update existing value in map_
+                it->second->value = value;
+                cache_.splice(cache_.begin(), cache_, it->second);
+                return;
+            }
+            if (map_.size() >= capacity_) {
+                // Remove LRU Item
+                Node lru = cache_.back();
+                map_.erase(lru.key);
+                cache_.pop_back();
+            }
+
+            // Insert new Item at front
+            cache_.push_front(Node(key, value));
+            map_[key] = cache_.begin();
+        }
+
+        bool contains(Key key) {
+            return map_.find(key) != map_.end();
+        }
+    }
+
+}
